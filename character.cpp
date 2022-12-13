333e7689533dca691f8d04b7af5db361c4a318f1
thibault luyts <Thibault.Luyts@student.pxl.be>
2022-12-13 10:49:21 +0100

    cleaned main

--- character.cpp
+++ character.cpp
@@ -2,7 +2,7 @@
 
 
 
-Character::Character(std::string name)
+Character::Character(std::string name): name(name)
 {
 
 }
@@ -10,4 +10,28 @@ Character::Character(std::string name)
 Character::~Character()
 {
     std::cout << "destruct Character" << name << std::endl;
+}
+std::string &Character::getName()
+{
+    return name;
+}
+
+bool Character::alive(bool live)
+{
+    return live;
+}
+
+bool Character::getLive()
+{
+    return live;
+}
+
+void Character::setLive(bool newLive)
+{
+    live = newLive;
+}
+
+std::string Character::setName(const std::string &newName)
+{
+    return(newName);
 }
